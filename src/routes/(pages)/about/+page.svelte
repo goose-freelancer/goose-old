f56f15121ac0c09257826706a812522ba707bfbe
Hugh <hughbrace@gmail.com>
2023-12-20 17:05:06 -0100

    Fix about page on tablet

--- src/routes/(pages)/about/+page.svelte
+++ src/routes/(pages)/about/+page.svelte
@@ -1,10 +1,10 @@
 
-<div class="grow overflow-hidden flex flex-col sm:grid sm:grid-cols-5 items-top gap-12">
-  <div class="sm:order-2 col-span-2 flex flex-col gap-4 items-center">
+<div class="grow overflow-hidden flex flex-col md:grid md:grid-cols-5 items-top gap-12">
+  <div class="md:order-2 col-span-2 flex flex-col gap-4 items-center">
     <img src="/images/tuleka.jpg" alt="Tuleka" class="">
     <a href="mailto:tuleka@goosefreelancer.com">tuleka@goosefreelancer.com</a>
   </div>
-  <div class="sm:order-1 col-span-3 h-full overflow-auto px-16">
+  <div class="md:order-1 col-span-3 h-full overflow-auto px-16">
     <p class="mb-8">
       Hi 👋🏾 I’m <a href="mailto:tuleka@goosefreelancer.com" class="underline">Tuleka!</a> Thanks for stopping by ☺️ Below is a brief backstory and explainer about what all this is about.
     </p>
