b120da278a444302db1a126d5aabf3ff321784b4
Hugh <hughbrace@gmail.com>
2024-01-13 13:26:07 -0400

    Left align text on mobile again

--- src/routes/(pages)/about/+page.svelte
+++ src/routes/(pages)/about/+page.svelte
@@ -5,31 +5,31 @@
     <a href="mailto:tuleka@goosefreelancer.com">tuleka@goosefreelancer.com</a>
   </div>
   <div class="md:order-1 col-span-3 h-full overflow-auto px-6">
-    <p class="mb-8 text-justify md:text-left">
+    <p class="mb-8 text-left">
       Hi 👋🏾 I’m <a href="https://www.linkedin.com/in/tuleka-prah-phd-55b22115a/" target="_blank" class="underline">Tuleka!</a> Thanks for stopping by 🙂 Below is a brief backstory and explainer about what all this is about.
     </p>
 
-    <p class="mb-8 text-justify md:text-left">
+    <p class="mb-8 text-left">
       <strong>Triggers & motivation</strong>: Prior to my recent role as a full-time employee at a startup, I’d been a freelancer in the creative industries for over 11 years. Those were some of the most rewarding and dynamic years of my employed life – I worked on so many interesting projects with lots of fascinating people and teams. In that time, tho, I also experienced a lot of stress and anxiety, stemming mainly from a couple recurring issues. I never really wanted to give up working as a freelancer, but those stressors eventually became a bit too much to bear.
     </p>
 
-    <p class="mb-8 text-justify md:text-left">
+    <p class="mb-8 text-left">
       Then a few months ago, whilst on parental leave, I found myself reflecting on potentially returning to freelancing. When I did, I felt all the anxieties creeping up again, which got me wondering about how I could instead affect positive change in the employment model that gave me so much happiness and freedom. I’m better-placed to do so nowadays because I can make full use of the combined skills, expertise and experience from my roles in the startup, with those from freelancing. I can put tried and tested strategic and leadership experience from taking projects and products from ideation to iteration, into developing and growing this proposed service. I’m energized by the challenge, and can’t think of a better way of directing my skills and expertise.
     </p>
 
-    <p class="mb-8 text-justify md:text-left">
+    <p class="mb-8 text-left">
       <strong>What’s next?</strong> We’d love to establish a service that supports and protects freelancers by directly addressing the key challenges faced by many in this world of work. But because what I’d experienced are my experiences, it won’t make sense to build something based on those alone; how can I propose to establish a service that addresses key pain points, without asking the community directly?
     </p>
 
-    <p class="mb-8 text-justify md:text-left">
+    <p class="mb-8 text-left">
       So, this is me doing that! I’d love your input on what your biggest challenges are when it comes to freelancing. <strong>What are your burdens – what causes you frustration, stress, and anxiety when it comes to being a freelancer?</strong> With your responses, we can develop a service that truly supports freelancers, because it’ll be built in direct collaboration and communication with the community.
     </p>
 
-    <p class="mb-8 text-justify md:text-left">
+    <p class="mb-8 text-left">
       <strong>So what happens now?</strong> If you click <a href="https://goosefreelancer.typeform.com/to/fm1nQBo8" target="_blank" class="underline">here</a>, you’ll be taken to a window where you’ll be asked to fill out a short survey about your experience as a freelancer.
     </p>
 
-    <p class="mb-8 text-justify md:text-left">
+    <p class="mb-8 text-left">
       Thanks a lot; your input is very much appreciated!
     </p>
   </div>
--- src/routes/de/(pages)/about/+page.svelte
+++ src/routes/de/(pages)/about/+page.svelte
@@ -6,31 +6,31 @@
   </div>
   <div class="md:order-1 col-span-3 h-full overflow-auto px-6">
 
-    <p class="mb-8 text-justify md:text-left">
+    <p class="mb-8 text-left">
       Hallo 👋🏾 Ich bin <a href="https://www.linkedin.com/in/tuleka-prah-phd-55b22115a/" target="_blank" class="underline">Tuleka</a>! Danke fürs Vorbeischauen 🙂 Nachfolgend eine kurze Vorgeschichte und Erklärung, worum es hier geht.
     </p>
 
-    <p class="mb-8 text-justify md:text-left">
+    <p class="mb-8 text-left">
       <strong>Auslöser und Motivation</strong>: Bevor ich vor kurzem als Vollzeitangestellte bei einem Startup anfing, war ich über 11 Jahre lang als Freiberuflerin in der Kreativbranche tätig. Das waren einige der lohnendsten und dynamischsten Jahre meines Berufslebens - ich habe an so vielen interessanten Projekten mit vielen faszinierenden Menschen und Teams gearbeitet. In dieser Zeit erlebte ich aber auch viel Stress und Ängste, die vor allem von einigen wiederkehrenden Problemen herrührten. Ich wollte meine freiberufliche Tätigkeit eigentlich nie aufgeben, aber diese Stressfaktoren wurden mir irgendwann zu viel.
     </p>
 
-    <p class="mb-8 text-justify md:text-left">
+    <p class="mb-8 text-left">
       Vor ein paar Monaten dann, als ich in Elternzeit war, habe ich über eine mögliche Rückkehr in die Selbständigkeit nachgedacht. Als ich das tat, spürte ich, wie all die Ängste wieder hochkamen, was mich dazu brachte, darüber nachzudenken, wie ich stattdessen das Beschäftigungsmodell, das mir so viel Freude und Freiheit gebracht hatte, positiv verändern könnte. Heute bin ich dazu besser in der Lage, weil ich die Fähigkeiten, das Fachwissen und die Erfahrung aus meiner Tätigkeit in einem Start-up mit denen aus der Freiberuflichkeit in vollem Umfang nutzen kann. Ich kann die bewährten strategischen und Führungserfahrungen, die ich bei der Entwicklung von Projekten und Produkten von der Idee bis zur Iteration gesammelt habe, in die Entwicklung und das Wachstum dieses vorgeschlagenen Service einbringen. Die Herausforderung reizt mich sehr, und ich kann mir keine bessere Möglichkeit vorstellen, meine Fähigkeiten und mein Fachwissen einzusetzen.
     </p>
 
-    <p class="mb-8 text-justify md:text-left">
+    <p class="mb-8 text-left">
       <strong>Was kommt als Nächstes?</strong> Wir würden gerne einen Service einrichten, der Freiberufler unterstützt und schützt, indem er direkt auf die wichtigsten Herausforderungen eingeht, mit denen viele in dieser Arbeitswelt konfrontiert sind. Aber da es sich bei meinen Erfahrungen um meine Erfahrungen handelt, macht es keinen Sinn, etwas zu entwickeln, das nur auf diesen Erfahrungen basiert. Wie kann ich einen Service vorschlagen, der die wichtigsten Probleme angeht, ohne die Community direkt zu fragen?
     </p>
 
-    <p class="mb-8 text-justify md:text-left">
+    <p class="mb-8 text-left">
       Und genau das tue ich jetzt! Ich würde mich freuen, wenn du mir sagst, was deine größten Herausforderungen bei der Freiberuflichkeit sind. <strong>Was belastet dich - was verursacht bei dir Frustration, Stress und Ängste, wenn du Freiberufler bist?</strong> Mit euren Antworten können wir einen Service entwickeln, der Freiberufler wirklich unterstützt, weil er in direkter Zusammenarbeit und Kommunikation mit der Community aufgebaut wird.
     </p>
 
-    <p class="mb-8 text-justify md:text-left">
+    <p class="mb-8 text-left">
       <strong>Was passiert also jetzt?</strong> Wenn du <a href="https://goosefreelancer.typeform.com/to/fm1nQBo8" target="_blank" class="underline">hier</a> klickst, wirst du zu einem Fenster weitergeleitet, in dem du gebeten wirst, eine kurze Umfrage über deine Erfahrungen als Freiberufler auszufüllen.
     </p>
 
-    <p class="mb-8 text-justify md:text-left">
+    <p class="mb-8 text-left">
       Vielen Dank; wir wissen eure Meinung sehr zu schätzen!
     </p>
   </div>
