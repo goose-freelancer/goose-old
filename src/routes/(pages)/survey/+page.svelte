484eb6c3a0084845c7802b0d63cb67bcad14b56e
Hugh <hughbrace@gmail.com>
2024-02-19 15:57:52 -0600

    Remove survey link

--- src/routes/(pages)/survey/+page.svelte
+++ src/routes/(pages)/survey/+page.svelte
@@ -2,37 +2,12 @@
 <div class="h-full flex flex-col justify-center items-center sm:grid sm:grid-cols-5 items-top gap-12">
   <div class="sm:col-span-3">
     <p class="mb-8 md:text-lg">
-      Thanks for your interest – we’re eager to learn about what your challenges as a freelancer are, and are excited to be given the opportunity to potentially address these. Please follow the prompts, and that’s basically it!
-    </p>
-
-    <p class="md:text-lg">
-      Through your contact email, we look forward to staying in touch about any progress or updates, and letting you know as soon as the service is ready to launch 🤗
+      This survey is now closed. Thank you for your support in taking it; we've had lots of great input, which will form the basis for the work we'll be doing over the next few months! We're updating this site, tho, so do check back again soon 😉
     </p>
   </div>
 
   <div class="sm:col-span-2 mb-8 grid grid-rows-4 gap-12">
-    <div class="row-span-3 flex flex-col justify-center">
-      <a
-        href="https://goosefreelancer.typeform.com/to/fm1nQBo8"
-        target="_blank"
-        class="
-          font-bold
-          text-xl
-          md:text-2xl
-          bg-mint-green
-          text-mighty-purple
-          p-4
-          md:p-8
-          md:mx-8
-          flex
-          text-center
-          items-center
-          justify-center
-        "
-      >
-        Quick Survey
-      </a>
-    </div>
+    <div class="row-span-3 flex flex-col justify-center" />
 
     <div class="fixed bottom-0 left-0 bg-mighty-purple md:static flex flex-row w-full items-center gap-2 lg:gap-4 border-t md:border-0 border-mint-green p-4">
       <a href="/" class="block grow bg-mint-green text-mighty-purple text-center py-4">Home</a>
--- src/routes/de/(pages)/survey/+page.svelte
+++ src/routes/de/(pages)/survey/+page.svelte
@@ -2,37 +2,12 @@
 <div class="h-full flex flex-col justify-center items-center sm:grid sm:grid-cols-5 items-top gap-12">
   <div class="sm:col-span-3">
     <p class="mb-8 md:text-lg">
-      Vielen Dank für dein Interesse – wir sind gespannt auf deine Herausforderungen als Freiberufler und freuen uns, dass du uns die Möglichkeit gibst, diese zu lösen. Bitte folgt den Aufforderungen, und das war's auch schon!
-    </p>
-
-    <p class="md:text-lg">
-      Wir freuen uns darauf, über deine Kontakt-E-Mail mit dir in Kontakt zu bleiben und dich über Fortschritte und Updates zu informieren, sobald der Dienst einsatzbereit ist 🤗
+      Diese Umfrage ist nun abgeschlossen. Vielen Dank für deine Unterstützung bei der Teilnahme an der Umfrage; wir haben viele tolle Beiträge erhalten, die die Grundlage für unsere Arbeit in den nächsten Monaten bilden werden! Wir sind dabei, diese Seite zu aktualisieren, also besucht uns doch bald wieder 😉
     </p>
   </div>
 
   <div class="sm:col-span-2 mb-8 grid grid-rows-4 gap-12">
-    <div class="row-span-3 flex flex-col justify-center">
-      <a
-        href="https://goosefreelancer.typeform.com/to/GiPNKtzS"
-        target="_blank"
-        class="
-          font-bold
-          text-xl
-          md:text-2xl
-          bg-mighty-purple
-          text-mint-green
-          p-4
-          md:p-8
-          md:mx-8
-          flex
-          text-center
-          items-center
-          justify-center
-        "
-      >
-        Kurze Umfrage
-      </a>
-    </div>
+    <div class="row-span-3 flex flex-col justify-center" />
 
     <div class="fixed bottom-0 left-0 bg-mint-green md:static flex flex-row w-full items-center gap-2 lg:gap-4 border-t md:border-0 border-mighty-purple p-4">
       <a href="/de" class="block grow bg-mighty-purple text-mint-green text-center py-4">Start</a>
